79d0a0887502081b7b738d5f4645047d5bbda87c
no9u93nuong2445-spec <no9u93nuong2445@gmail.com>
2026-07-26 23:44:37 +0800

    添加无需运行时解压的直接程序入口

---
+++ app.js
@@ -0,0 +1,19 @@
+---
+layout: null
+---
+{% include_relative fragments/app-01.txt %}
+{% include_relative fragments/app-02.txt %}
+{% include_relative fragments/app-03.txt %}
+{% include_relative fragments/app-04.txt %}
+{% include_relative fragments/app-05.txt %}
+{% include_relative fragments/app-06.txt %}
+{% include_relative fragments/app-07.txt %}
+{% include_relative fragments/app-08.txt %}
+{% include_relative fragments/app-09.txt %}
+{% include_relative fragments/app-10.txt %}
+{% include_relative fragments/app-11.txt %}
+{% include_relative fragments/app-12.txt %}
+{% include_relative fragments/app-13.txt %}
+{% include_relative fragments/app-14.txt %}
+{% include_relative fragments/app-15.txt %}
+{% include_relative fragments/app-16.txt %}
